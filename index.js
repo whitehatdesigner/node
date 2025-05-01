0bbdb1b84a043cbc5017f7cbf8eda7038ebf3689
whitehatdesigner <ankitshukla828774@gmail.com>
2025-05-02 02:16:36 +0530

    ...

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { name } = require('ejs');
 const express = require('express');
 
 const app = express();
@@ -12,15 +13,9 @@ app.get('/' , (req, res) => {
     res.json(users)
 })
 
-
-app.get('/about', (req, res) => {
-    res.redirect('/user')
-})
-
-
-app.get('/user', (req, res) => {
-    res.send("<h1>This is user page</h1>")
-})
+// app.get('/user', (req, res) => {
+//     res.send("<h1>This is user page 2</h1>")
+// })
 // const ankit = 20;
 
 // app.get(`/about/user/${ankit}`, (req, res) => {
@@ -38,3 +33,29 @@ app.get('/user', (req, res) => {
 // app.get('/users/search', (req, res) => {
 //     res.send(req.query);
 // })
+
+// ============= rander page ================
+
+app.set('view engine', 'ejs')
+app.get('/about-us', (req, res) => {
+    res.render('about-us');
+})
+
+// app.get('/download', (req, res) => {
+//     res.download('./uploads/node-img.webp', 'new-img.webp')
+// })
+
+// app.get('/download', (req, res) => {
+//     res.sendFile(__dirname + '/uploads/node-img.webp')
+// })
+
+// app.get('/user', (req, res) => {
+//     res.set('namert', 'ankit shukla');
+//     console.log();
+//     res.send(res.get('namert'))
+// })
+
+app.get('/error', (req, res) => {
+    res.header(404);
+    res.end()
+})
