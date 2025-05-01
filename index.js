82212343f14611197b66cd5bf95788da3acf9ac3
whitehatdesigner <ankitshukla828774@gmail.com>
2025-05-01 23:30:47 +0530

    ...

--- index.js
+++ index.js
@@ -5,28 +5,36 @@ const app = express();
 app.listen('7000');
 
 app.get('/' , (req, res) => {
-    res.send("<h1>Welcome to home page</h1>")
+    const users = [
+        {name: 'a', age: 20},
+        {name: 'b', age: 30}
+    ]
+    res.json(users)
 })
 
 
 app.get('/about', (req, res) => {
-    res.send("<h1>About Page</h1>")
+    res.redirect('/user')
 })
 
-const ankit = 20;
 
-app.get(`/about/user/${ankit}`, (req, res) => {
-    res.send("<h1>About Page with user</h1>")
+app.get('/user', (req, res) => {
+    res.send("<h1>This is user page</h1>")
 })
+// const ankit = 20;
 
-app.get(`/about/user/:id`, (req, res) => {
-    res.send(req.params)
-})
+// app.get(`/about/user/${ankit}`, (req, res) => {
+//     res.send("<h1>About Page with user</h1>")
+// })
 
-app.get('/user/:id/book/:bookid', (req, res) => {
-    res.send(`Book Id : ${req.params.bookid}`)
-})
+// app.get(`/about/user/:id`, (req, res) => {
+//     res.send(req.params)
+// })
 
-app.get('/users/search', (req, res) => {
-    res.send(req.query);
-})
+// app.get('/user/:id/book/:bookid', (req, res) => {
+//     res.send(`Book Id : ${req.params.bookid}`)
+// })
+
+// app.get('/users/search', (req, res) => {
+//     res.send(req.query);
+// })
